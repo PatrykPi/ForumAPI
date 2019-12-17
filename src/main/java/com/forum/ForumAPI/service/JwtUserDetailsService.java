2e331f29fb26578f85512dd9a58193525ca0662a
Patryk <piskorz.patryk.96@gmail.com>
2019-12-17 16:55:43 +0100

    Added JwtUserDetailsService

---
+++ src/main/java/com/forum/ForumAPI/service/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.forum.ForumAPI.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class JwtUserDetailsService implements UserDetailsService {
+	
+	public final String USER_NAME = "javainuse";
+	public final String PASSWORD = "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6";
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if (USER_NAME.equals(username)) {
+			return new User(USER_NAME, PASSWORD, new ArrayList<>());
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+	}
+
+}
