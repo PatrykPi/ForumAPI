827e7134a400a1db000fa42b085b41d41511e79f
Patryk <piskorz.patryk.96@gmail.com>
2019-12-18 19:03:12 +0100

    Lombok annotations up

--- src/main/java/com/forum/ForumAPI/entity/UserEntity.java
+++ src/main/java/com/forum/ForumAPI/entity/UserEntity.java
@@ -9,8 +9,13 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "user")
+@Getter
+@Setter
 public class UserEntity {
 	
 	@Id
@@ -23,20 +28,4 @@ public class UserEntity {
 	@Column
 	@JsonIgnore
 	private String password;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
