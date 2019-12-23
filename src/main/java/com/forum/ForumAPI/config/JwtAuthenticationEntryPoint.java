d51ea2b08322741801aa7ed21f263b32098c8999
Patryk <piskorz.patryk.96@gmail.com>
2019-12-23 17:57:23 +0100

    Error response in AuthenticationEntryPoint

--- src/main/java/com/forum/ForumAPI/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/forum/ForumAPI/config/JwtAuthenticationEntryPoint.java
@@ -2,23 +2,50 @@ package com.forum.ForumAPI.config;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.forum.ForumAPI.model.ErrorResponseBody;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
 	
 	private static final long serialVersionUID = -8921155984364483551L;
+	
+	@Autowired
+	private ObjectMapper ObjMapper;
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException {
 		
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+		ErrorResponseBody body = new ErrorResponseBody();
+		
+		List<String> errors = new ArrayList<>();
+		
+		errors.add(authException.getMessage());
+		
+		body.setErrors(errors);
+		
+		String bodyJsonString = this.ObjMapper.writeValueAsString(body);
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        
+        response.getWriter().print(bodyJsonString);;
+		
+//		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//		PrintWriter writer = response.getWriter();
+//		writer.println("HTTP Status 401 - " + authException.getMessage());
 	}
 }
