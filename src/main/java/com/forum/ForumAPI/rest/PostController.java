21b3f44f60c1523e18f3172611cfe38b6dda27ec
Patryk <piskorz.patryk.96@gmail.com>
2020-01-02 21:05:30 +0100

    Update

--- src/main/java/com/forum/ForumAPI/rest/PostController.java
+++ src/main/java/com/forum/ForumAPI/rest/PostController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.forum.ForumAPI.entity.PostEntity;
@@ -26,6 +28,14 @@ public class PostController {
 		PostEntity post = postService.findByIdWithPublicAccess(postId);
 		
 		return ResponseEntity.ok(post);
+	}
+	
+	@PatchMapping("/{postId}")
+	public ResponseEntity<?> updatePost(@RequestParam(required = false) boolean isLiked, 
+			@RequestParam(required = false) boolean isDisliked){
+		
+		return null;
+		
 	}
 
 }
